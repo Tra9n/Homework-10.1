0b8ca917f59563281507b280c3d838af1d29bdb2
pro10 <pro100.prometey@mail.ru>
2026-05-11 20:37:40 +0300

    Готовое_Дз_10.1.

--- src/processing.py
+++ src/processing.py
@@ -11,6 +11,8 @@ def filter_by_state(library: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(list_dir: list, descending: bool = True) -> list:
-    """Функция должна возвращать новый список, отсортированный по дате (date)."""
+    """Функция должна возвращать новый список,
+     отсортированный по дате (date).
+    """
     sorted_library = sorted(list_dir, key=lambda k: k["date"], reverse=descending)
     return sorted_library
