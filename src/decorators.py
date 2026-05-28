f836a0c2d21a57206927d00804cdfff24bd82231
pro10 <pro100.prometey@mail.ru>
2026-05-28 23:53:59 +0300

    Дз_11.2_

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from typing import Any, Callable, Optional
+
+
+def log(filename: Optional[str] = None) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: tuple, **kwargs: dict) -> Any:
+            def write_log(message: str) -> None:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as log_file:
+                        log_file.write(message + "\n")
+                else:
+                    print(message)
+
+            write_log(f"Вызвана функция {func.__name__}")
+
+            try:
+                result = func(*args, **kwargs)
+                write_log(f"{func.__name__} - OK - {result}")
+                return result
+
+            except Exception as e:
+                write_log(f"{func.__name__} - {type(e).__name__} - args: {args}, kwargs: {kwargs}")
+
+                raise
+
+        return wrapper
+
+    return decorator
